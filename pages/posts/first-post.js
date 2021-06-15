cd993675bad38c6a7c9dc7dced61579f0031c488
RAL <rob@robecommerce.com>
2021-06-15 10:42:03 -0500

    new dev env

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+          <Link href="/">
+          <a>Back to home</a>
+        </Link>
+    
+    </>
+  );
+}
