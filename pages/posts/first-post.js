ab34c6f41d029ecb313b4046d616ad77e76dfc21
RAL <rob@robecommerce.com>
2021-06-18 09:08:48 -0500

    next commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 export default function FirstPost() {
   return (
     <>
+          <p>hola robbie</p>
           <Link href="/">
           <a>Back to home</a>
         </Link>
